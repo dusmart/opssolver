8e472908601ee4fcf8e8060076904cdbb45370d6
dusmart <dusmart@work.local>
2021-10-12 10:44:58 +0800

    log

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    print("0ops:print")
+    logging.error("0ops:logging.error")
     return "Hello, World!"
