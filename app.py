c44b11e0aabec15096e47227d16568d67e85e250
dusmart <dusmart@work.local>
2021-10-12 10:54:05 +0800

    print header

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    logging.error(json.dumps(request))
+    logging.error(request)
+    logging.error(request.headers)
+    logging.error(request.remote_addr)
     return "Hello, World!"
